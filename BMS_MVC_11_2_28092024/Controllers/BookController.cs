349ffa48bbd2c207f47027229118ee847359d54a
lenovo <lenovo@DESKTOP-G3RV5V6>
2024-10-19 13:36:46 +0530

    File upload and DDL binding

---
+++ BMS_MVC_11_2_28092024/Controllers/BookController.cs
@@ -0,0 +1,56 @@
+﻿using BMS_MVC_11_2_28092024.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Mvc;
+using BMS_MVC.DataAccess.Repositories;
+using BMS_MVC.DataAccess.Entity;
+
+namespace BMS_MVC_11_2_28092024.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+
+        private CategoryRepository categoryRepository;
+        public BookController()
+        {
+            categoryRepository = new CategoryRepository();
+        }
+        public ActionResult Index()
+        {
+           return View();
+        }
+
+
+        public ActionResult Create()
+        {
+            List<CategoryEntity> catlist = categoryRepository.Get();
+            BookModel model = new BookModel()
+            {
+                CategoryList = catlist.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.CategoryId.ToString()
+                }).ToList()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(BookModel model)
+        {
+            if (model.CoverPhoto != null)
+            {
+                string ext = Path.GetExtension(model.CoverPhoto.FileName);
+                string fileName = DateTime.Now.ToString("ddmmyyyy-") + Guid.NewGuid().ToString()+ext;
+                string directory = "~/Resources/Image";
+                string filePath  = Path.Combine(Server.MapPath(directory), fileName);
+                model.CoverPhoto.SaveAs(filePath);
+            }
+            return View();
+        }
+    }
+}
---
+++ BMS_MVC_11_2_28092024/Models/BookModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BMS_MVC_11_2_28092024.Models
+{
+    public class BookModel
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public int  Category { get; set; }
+
+        public List<SelectListItem> CategoryList { get; set; }
+        public HttpPostedFileBase CoverPhoto { get; set; }
+    }
+}
